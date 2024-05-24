59c5e79a89795d1cd3899007c5ae9b8ab182f847
Serdar Vural <serdarvural740@gmail.com>
2024-05-24 23:54:18 +0300

    project

---
+++ busticket_project/busticket_project/employeesignup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace busticket_project
+{
+    public partial class employeesignup : Form
+    {
+        public employeesignup()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=byticket.accdb;");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //KULLANICI KAYIT ETME 
+            if (textBox4.Text == textBox5.Text)
+            {
+                connection.Open();
+                OleDbCommand komut = new OleDbCommand("insert into companyemployee(employeename,employeesurname,employeeusername,employeeuserPassword) values (@en,@esn,@eun,@eup)", connection);
+
+                komut.Parameters.AddWithValue("@en", textBox1.Text);
+                komut.Parameters.AddWithValue("@es", textBox2.Text);
+                komut.Parameters.AddWithValue("@eun", textBox3.Text);
+                komut.Parameters.AddWithValue("@ep", textBox4.Text);
+                komut.ExecuteNonQuery();
+
+                connection.Close();
+                MessageBox.Show("RECORDED");
+
+
+                homepage hp= new homepage();
+                hp.Show();
+                this.Hide();
+            }
+
+            else
+            {
+                MessageBox.Show("Your password and password confirmation do not match. Please enter the same password.");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            homepage hp = new homepage();
+            hp.Show();
+            this.Hide();
+        }
+
+        private void employeesignup_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
